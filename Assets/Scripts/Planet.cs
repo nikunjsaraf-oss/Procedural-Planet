7baf1f4d834a118e8ff4c226db40b998cbc7d57b
Nikunj Saraf <nikunjsaraf2001@gmail.com>
2021-11-09 19:56:21 +0530

    Create Sphere

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    [Range(2, 256)] public int resolution = 10;
+    
+    [SerializeField, HideInInspector]
+    private MeshFilter[] meshFilters;
+    private TerrainFace[] _terrainFaces;
+
+    private void OnValidate()
+    {
+        Initialize();
+        GenerateMesh();
+    }
+
+    void Initialize()
+    {
+        if (meshFilters == null || meshFilters.Length == 0)
+        {
+            meshFilters = new MeshFilter[6];
+        }
+
+        _terrainFaces = new TerrainFace[6];
+
+        Vector3[] directions =
+        {
+            Vector3.up,
+            Vector3.down,
+            Vector3.left,
+            Vector3.right,
+            Vector3.forward,
+            Vector3.back
+        };
+
+        for (var i = 0; i < 6; i++)
+        {
+            if (meshFilters[i] == null)
+            {
+                var meshObj = new GameObject("Mesh")
+                {
+                    transform =
+                    {
+                        parent = transform
+                    }
+                };
+
+                meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+                meshFilters[i] = meshObj.AddComponent<MeshFilter>();
+                meshFilters[i].sharedMesh = new Mesh();
+            }
+
+            _terrainFaces[i] = new TerrainFace(meshFilters[i].sharedMesh, resolution, directions[i]);
+        }
+    }
+
+    void GenerateMesh()
+    {
+        foreach (var face in _terrainFaces)
+        {
+            face.ConstructMesh();
+        }
+    }
+}
---
+++ Assets/Scripts/TerrainFace.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class TerrainFace
+{
+    private Mesh _mesh;
+    private int _resolution;
+    private Vector3 _localUp;
+    private Vector3 _axisA;
+    private Vector3 _axisB;
+
+    public TerrainFace(Mesh mesh, int resolution, Vector3 localUp)
+    {
+        _mesh = mesh;
+        _resolution = resolution;
+        _localUp = localUp;
+
+        _axisA = new Vector3(localUp.y, localUp.z, localUp.x);
+        _axisB = Vector3.Cross(localUp, _axisA);
+    }
+
+    public void ConstructMesh()
+    {
+        var vertices = new Vector3[_resolution * _resolution];
+        var triangles = new int[(_resolution - 1) * (_resolution - 1) * 6];
+        var triangleIndex = 0;
+
+        for (var y = 0; y < _resolution; y++)
+        {
+            for (var x = 0; x < _resolution; x++)
+            {
+                var i = x + y * _resolution;
+                var percent = new Vector2(x, y) / (_resolution - 1);
+                var pointOnCube = _localUp + (percent.x - 0.5f) * 2 * _axisA + (percent.y - 0.5f) * 2 * _axisB;
+                var pointOnUnitSphere = pointOnCube.normalized;
+                vertices[i] = pointOnUnitSphere;
+
+                if (x == _resolution - 1 || y == _resolution - 1) continue;
+                triangles[triangleIndex] = i;
+                triangles[triangleIndex + 1] = i + _resolution + 1;
+                triangles[triangleIndex + 2] = i + _resolution;
+
+                triangles[triangleIndex + 3] = i;
+                triangles[triangleIndex + 4] = i + 1;
+                triangles[triangleIndex + 5] = i + _resolution + 1;
+
+                triangleIndex += 6;
+            }
+        }
+        
+        _mesh.Clear();
+        _mesh.vertices = vertices;
+        _mesh.triangles = triangles;
+        _mesh.RecalculateNormals();
+    }
+}
